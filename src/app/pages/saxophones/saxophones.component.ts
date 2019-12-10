07477951085050df93a7ec00208c0e8e17fb536d
HuangPeifeng <35569356+HuangPeifeng@users.noreply.github.com>
2019-12-10 22:47:28 +0800

    修正薩克斯風內容

--- src/app/pages/saxophones/saxophones.component.ts
+++ src/app/pages/saxophones/saxophones.component.ts
@@ -8,19 +8,54 @@ import { Component, OnInit } from '@angular/core';
 export class SaxophonesComponent implements OnInit {
   menu = [
     { name: '全部' },
-    { name: '超高音' },
     { name: '高音' },
-    { name: '中音' },
-    { name: '次中音' },
-    { name: '上低音' }
+    { name: '中音' }
   ];
+  list = [
+    { category: '高音', fileName: 'sax-high1.jpg' },
+    { category: '高音', fileName: 'sax-high2.jpg' },
+    { category: '中音', fileName: 'sax-mid1.jpg' },
+    { category: '中音', fileName: 'sax-mid2.jpg' },
+    { category: '中音', fileName: 'sax-mid3.jpg' },
+    { category: '中音', fileName: 'sax-mid4.jpg' },
+    { category: '中音', fileName: 'sax-mid5.jpg' },
+    { category: '中音', fileName: 'sax-mid6.jpg' }
+  ];
+  subList = this.list;
+
   selectedIndex = 0;
 
   isPopupOpen = false;
+  selected = null;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeCategory(item, index) {
+    this.selectedIndex = index;
+    switch (item.name) {
+      case '高音':
+        this.subList = this.list.filter(res => res.category === '高音');
+        break;
+      case '中音':
+        this.subList = this.list.filter(res => res.category === '中音');
+        break;
+      default:
+        this.subList = this.list;
+        break;
+    }
+  }
+
+  openPopup(item) {
+    this.isPopupOpen = true;
+    this.selected = item;
+  }
+
+  closePopup() {
+    this.isPopupOpen = false;
+    this.selected = null;
   }
 
 }
