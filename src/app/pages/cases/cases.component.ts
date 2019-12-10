56f6f779c2527b8c0077a00120c684cf15b77489
HuangPeifeng <35569356+HuangPeifeng@users.noreply.github.com>
2019-12-10 22:50:54 +0800

    修正箱子內容

--- src/app/pages/cases/cases.component.ts
+++ src/app/pages/cases/cases.component.ts
@@ -12,13 +12,31 @@ export class CasesComponent implements OnInit {
     { name: '硬質ABS造型箱' },
     { name: '硬質帆布造型箱' }
   ];
+  list = [
+    { url: 'case1.jpg' },
+    { url: 'case2.jpg' },
+    { url: 'case3.jpg' },
+    { url: 'case4.jpg' },
+    { url: 'case5.jpg' },
+  ];
   selectedIndex = 0;
+  selected = null;
 
   isPopupOpen = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openPopup(item) {
+    this.isPopupOpen = true;
+    this.selected = item;
+  }
+
+  closePopup() {
+    this.isPopupOpen = false;
+    this.selected = null;
   }
 
 }
