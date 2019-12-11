d2f18028ed4e5c9890b19de8ce79ef40b4d9f50a
HuangPeifeng <35569356+HuangPeifeng@users.noreply.github.com>
2019-12-11 20:48:16 +0800

    修正背景

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  cover = '/assets/images/1.jpg';
+  cover = '/assets/images/sax/sax-high1.jpg';
   count = 0;
-  list = [1, 2, 3, 4, 5, 6];
+  list = [1, 2, 3, 4, 5];
   opacity = 1;
   $interval = null;
 
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.$interval = setInterval(() => {
       this.count++;
-      if (this.count > 5) {
+      if (this.count > 4) {
         this.count = 0;
       }
       const isOpacity = setInterval(() => {
@@ -25,7 +25,7 @@ export class HomeComponent implements OnInit, OnDestroy {
         if (this.opacity < 0) {
           this.opacity = 0;
           clearInterval(isOpacity);
-          this.cover = `/assets/images/${this.list[this.count]}.jpg`;
+          this.cover = `/assets/images/sax/sax-high${this.list[this.count]}.jpg`;
           const noOpacity = setInterval(() => {
             this.opacity = this.opacity + 0.1;
             if (this.opacity > 1) {
