10a8bea82365e30bf74971158a367f51d1a48dce
HuangPeifeng <35569356+HuangPeifeng@users.noreply.github.com>
2019-12-17 20:08:17 +0800

    工廠及組裝零配件 翻譯

--- src/app/pages/accessories/accessories.module.ts
+++ src/app/pages/accessories/accessories.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AccessoriesRoutingModule } from './accessories-routing.module';
 import { AccessoriesComponent } from './accessories.component';
+import { ThemeModule } from 'src/app/theme/theme.module';
 
 
 @NgModule({
   declarations: [AccessoriesComponent],
   imports: [
     CommonModule,
-    AccessoriesRoutingModule
+    AccessoriesRoutingModule,
+    ThemeModule
   ]
 })
 export class AccessoriesModule { }
